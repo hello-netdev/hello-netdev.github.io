3344458bd966d28a9b72b0e3ea5d63b19319c061
Xiao-Jiang-233 <122152524+Xiao-Jiang-233@users.noreply.github.com>
2024-11-27 21:39:25 +0800

    ✨ feat: 添加个deploy的脚步

---
+++ mkgh.ps1
@@ -0,0 +1,12 @@
+﻿# 获取脚本的完整路径
+$scriptPath = $MyInvocation.MyCommand.Definition
+ 
+# 获取脚本所在的目录
+$scriptDir = Split-Path $scriptPath
+ 
+# 设置当前目录为脚本目录
+Set-Location $scriptDir
+
+mkdocs gh-deploy --config-file ../hello-netdev/mkdocs.yml --remote-branch main
+git reset
+git clean -fd
